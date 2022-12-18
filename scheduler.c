2a567be74da5160025a13a60e35d5f10fc77c150
ranamoataz3 <ranamoataz02@gmail.com>
2022-12-18 18:36:37 +0200

    part1 of scheduler and input file

--- scheduler.c
+++ scheduler.c
@@ -1,11 +1,21 @@
-#include "headers.h"
+#include "DataStructure.c"
+
+// argument list (./scheduler.out , scheduling algo number , number of process , quantam if exist)
 
 int main(int argc, char *argv[])
 {
     initClk();
 
-    //TODO: implement the scheduler.
-    //TODO: upon termination release the clock resources.
+    int sch = atoi(argv[1]);
+    int no_processes = atoi(argv[2]);
+
+    key_t key_id;
+    int msgq_id, send_approve;
+    struct msgbuff message_buffer;
+    message_buffer.mtype = no_processes;
+
+    key_id = ftok("sendProcess", 65);           // create unique key
+    msgq_id = msgget(key_id, 0666 | IPC_CREAT); // create message queue and return id
 
     destroyClk(true);
 }
