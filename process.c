9e2687f26042539dc7524e016c0401d3ced8233f
Donia Remah <doniaremah@gmail.com>
2022-12-22 19:18:41 +0200

    started in process.c

--- process.c
+++ process.c
@@ -5,15 +5,31 @@ int remainingtime;
 
 int main(int agrc, char *argv[])
 {
+
     initClk();
 
-    //TODO The process needs to get the remaining time from somewhere
-    //remainingtime = ??;
+    remainingtime = atoi(argv[0]);
+
+    printf("Process with id: %d is started at time %d. \nRemaning Time is: %d \n",getpid(),getClk(),remainingtime);
+    
+    int nowclk;
+
+    //TODO The process needs to get the remaining time from schedular from args.
     while (remainingtime > 0)
     {
-        // remainingtime = ??;
+        // Ex: getclk = 1, nowclk = 1
+        nowclk = getClk(); // getting time at begining of each loop
+
+        // pausing till clock moves 1 second.
+        while (nowclk == getClk()){} 
+
+        remainingtime--;
+
     }
 
+    printf("Process with id: %d is terminating at time %d. \nRemaning Time is: %d \n",getpid(),getClk(),remainingtime);
+    // if remaning time =0, send to schedular process is finished.
+    kill(getppid(),SIGUSR1); 
     destroyClk(false);
 
     return 0;
